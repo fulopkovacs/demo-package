bb0128ad8927c0fa427b5f59fc24b564a4e76e6f
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-04-28 22:49:01 +0200

    Try to upload to the package to a DO space

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -8,29 +8,23 @@ set -o nounset
 # is the name of the package
 PACKAGE_NAME="$1"
 
-echo "$PACKAGE_NAME"
-
-echo "$KEY"
-
-exit 0
-
 # Reference: https://docs.digitalocean.com/reference/api/spaces-api/
 
 # Step 1: Define the parameters for the Space you want to upload to.
-SPACE="nyc-tutorial-space" # Find your endpoint in the control panel, under Settings.
-REGION="nyc3"              # Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
-STORAGETYPE="STANDARD"     # Storage type, can be STANDARD, REDUCED_REDUNDANCY, etc.
-KEY="5SGMECSBJ6UPVC2AJ6B4" # Access key pair. You can create access key pairs using the control panel or API.
-SECRET="$SECRET"           # Secret access key defined through an environment variable.
+SPACE="my-npm-packages" # Find your endpoint in the control panel, under Settings.
+REGION="fra1"           # Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
+STORAGETYPE="STANDARD"  # Storage type, can be STANDARD, REDUCED_REDUNDANCY, etc.
+KEY="${KEY}"            # Access key pair. You can create access key pairs using the control panel or API.
+SECRET="$SECRET"        # Secret access key defined through an environment variable.
 
 # Step 2: Define a function that uploads your object via cURL.
 function putS3 {
 	path="."               # The local path to the file you want to upload.
-	file="hello-world.txt" # The file you want to upload.
+	file="${PACKAGE_NAME}" # The file you want to upload.
 	space_path="/"         # The path within your Space where you want to upload the new file.
 	space="${SPACE}"
 	date=$(date +"%a, %d %b %Y %T %z")
-	acl="x-amz-acl:private"   # Defines Access-control List (ACL) permissions, such as private or public.
+	acl="x-amz-acl:public"    # Defines Access-control List (ACL) permissions, such as private or public.
 	content_type="text/plain" # Defines the type of content you are uploading.
 	storage_type="x-amz-storage-class:${STORAGETYPE}"
 	string="PUT\n\n$content_type\n$date\n$acl\n$storage_type\n/$space$space_path$file"
