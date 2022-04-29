a2591c4973f6aaf3af8220b17446a29432659ab2
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-04-29 09:58:31 +0200

    Next try

---
+++ scripts/publish-package.js
@@ -0,0 +1,63 @@
+// Step 1: Import the S3Client object and all necessary SDK commands.
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const fs = require("fs");
+
+// Step 2: The s3Client function validates your request and directs it to your Space's specified endpoint using the AWS SDK.
+const s3Client = new S3Client({
+  endpoint: "https://fra1.digitaloceanspaces.com", // Find your endpoint in the control panel, under Settings. Prepend "https://".
+  region: "fra1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
+  credentials: {
+    accessKeyId: process.env.KEY, // Access key pair. You can create access key pairs using the control panel or API.
+    secretAccessKey: process.env.SECRET, // Secret access key defined through an environment variable.
+  },
+});
+
+const localFile = "demo-package-v1.0.0.tgz";
+const fileStream = fs.readFileSync(localFile);
+
+// Step 3: Define the parameters for the object you want to upload.
+const s3Params = {
+  Bucket: "my-npm-packages", // The path to the directory you want to upload the object to, starting with your Space name.
+  Key: process.env.PKG_NAME, // Object key, referenced whenever you want to access this file later.
+  // Body: "Hello, World!", // The object's contents. This variable is an object, not a string.
+  ACL: "public", // Defines ACL permissions, such as private or public.
+  Body: fileStream,
+  Metadata: {
+    // Defines metadata tags.
+    "x-amz-meta-my-key": "your-value",
+  },
+};
+
+// Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
+const uploadObject = async () => {
+  try {
+    const data = await s3Client.send(new PutObjectCommand(s3Params));
+    console.log(
+      "Successfully uploaded object: " + s3Params.Bucket + "/" + s3Params.Key
+    );
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+// Step 5: Call the uploadObject function.
+uploadObject();
+
+// var client = s3.createClient(s3Client);
+/* console.log(s3Client.send);
+var uploader = s3Client.uploadFile({ s3Params, localFile });
+uploader.on("error", function (err) {
+  console.error("unable to upload:", err.stack);
+});
+uploader.on("progress", function () {
+  console.log(
+    "progress",
+    uploader.progressMd5Amount,
+    uploader.progressAmount,
+    uploader.progressTotal
+  );
+});
+uploader.on("end", function () {
+  console.log("done uploading");
+}); */
